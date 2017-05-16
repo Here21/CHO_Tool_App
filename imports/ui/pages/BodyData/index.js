83f72a9c7dd697e2810fce4b7d511d35083a7ce3
Here21 <deletetmr@gmail.com>
2017-05-16 22:37:47 +0800

    bodydata page

--- imports/ui/pages/BodyData/index.js
+++ imports/ui/pages/BodyData/index.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
+import FunctionCard from '../../components/FunctionCard';
+
+import './style.scss';
+
+const cardContent = (
+  <div>
+    <div>
+      <span style={{ marginRight: '3px', fontSize: '9px' }}>剩余</span>
+      <span style={{ fontSize: '32px', verticalAlign: 'middle' }}>31</span>
+      <span style={{ verticalAlign: 'super', fontSize: '9px' }}>次</span>
+    </div>
+    <div>
+      <span style={{ marginRight: '3px', fontSize: '9px' }}>剩余</span>
+      <span style={{ fontSize: '32px', verticalAlign: 'middle' }}>31</span>
+      <span style={{ verticalAlign: 'super', fontSize: '9px' }}>次</span>
+    </div>
+    <div>
+      <span style={{ marginRight: '3px', fontSize: '9px' }}>剩余</span>
+      <span style={{ fontSize: '32px', verticalAlign: 'middle' }}>31</span>
+      <span style={{ verticalAlign: 'super', fontSize: '9px' }}>次</span>
+    </div>
+  </div>
+);
 
 class BodyData extends Component {
   render() {
     return (
-      <div>sdfsdf</div>
+      <div className="body-page">
+        <div className="top-part">
+          <div className="shadow">
+            <img src="" alt=""/>
+          </div>
+          <button>添加计划</button>
+        </div>
+        <FunctionCard />
+        <FunctionCard
+          img="http://op99wrlxr.bkt.clouddn.com/image/icons/plan_64.png"
+          title="碳氢氧计划"
+          content={
+            cardContent
+          }
+        />
+      </div>
     );
   }
 }
